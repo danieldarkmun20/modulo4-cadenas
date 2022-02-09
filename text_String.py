4488ab02593283b3006173c7a2ebba2d84b84931
DanielDarkmun <dape_18@alu.uabcs.mx>
2022-02-08 20:00:14 -0700

    Actrividad finalizada

--- text_String.py
+++ text_String.py
@@ -1,3 +1,18 @@
+#Ejercicio 1
+
+text = """Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth. 
+On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest"""
+texts_array = text.split('.')
+
+keywords = ['average','temperature', 'distance']
+
+for text in texts_array:
+    for keyword in keywords:
+        if keyword in text:
+            print(text.replace(' C', ' celsius'))
+            break
+
+#Ejercicio 2
 # Datos con los que vas a trabajar
 name = "Moon"
 gravity = 0.00162 # in kms
